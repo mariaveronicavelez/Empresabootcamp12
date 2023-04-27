92a27d8e2dd1afc8dcb2945ffa66b52968a52fa0
veronica.velez <mavevelez9@gmail.com>
2023-04-27 11:59:15 -0300

    "Se agrga datos del cliente"

---
+++ src/Modelo/DatosCliente.java
@@ -0,0 +1,22 @@
+package Modelo;
+
+public class DatosCliente {
+    private String nombre;
+    private int rut;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+}
