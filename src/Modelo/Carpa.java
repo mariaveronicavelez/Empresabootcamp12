cba46af68357f78adab5e5ffd260af0b02cf0656
veronica.velez <mavevelez9@gmail.com>
2023-04-27 11:58:33 -0300

    "Se modifica el modelo carpa"

---
+++ src/Modelo/Carpa.java
@@ -0,0 +1,13 @@
+package Modelo;
+
+public class Carpa extends MediodeAlojamiento{
+    private Integer cantidaddepersonas;
+
+    public Integer getCantidaddepersonas() {
+        return cantidaddepersonas;
+    }
+
+    public void setCantidaddepersonas(Integer cantidaddepersonas) {
+        this.cantidaddepersonas = cantidaddepersonas;
+    }
+}
