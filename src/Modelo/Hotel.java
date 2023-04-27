d4af47e2bc88646826da3d7c007b9400adfbf8f7
veronica.velez <mavevelez9@gmail.com>
2023-04-27 12:01:41 -0300

    "Se actualiza el modelo hotel"

---
+++ src/Modelo/Hotel.java
@@ -0,0 +1,14 @@
+package Modelo;
+
+public class Hotel extends Hospederia {
+    private boolean condesayuno;
+
+
+    public boolean isCondesayuno() {
+        return condesayuno;
+    }
+
+    public void setCondesayuno(boolean condesayuno) {
+        this.condesayuno = condesayuno;
+    }
+}
