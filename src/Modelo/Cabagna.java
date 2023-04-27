bfb0d918b4a4b2333439fb37cd2484b8514db4dc
veronica.velez <mavevelez9@gmail.com>
2023-04-27 11:58:00 -0300

    "Se modifica el modelo cabagna"

---
+++ src/Modelo/Cabagna.java
@@ -0,0 +1,24 @@
+package Modelo;
+
+public class Cabagna extends Hospederia {
+    private boolean chimenea;
+    private  Integer capacidad;
+
+    public boolean isChimenea() {
+        return chimenea;
+    }
+
+    public void setChimenea(boolean chimenea) {
+        this.chimenea = chimenea;
+    }
+
+    @Override
+    public Integer getCapacidad() {
+        return capacidad;
+    }
+
+    @Override
+    public void setCapacidad(Integer capacidad) {
+        this.capacidad = capacidad;
+    }
+}
